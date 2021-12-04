9a88abfa6215a7e66812185370920375f4a59747
Meet Jain <93723420+MeetJain20@users.noreply.github.com>
2021-12-04 10:17:03 +0530

    Add files via upload

---
+++ MainFile.java
@@ -0,0 +1,52 @@
+
+//Header Files
+import LoginPage.*;
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+class MainFile
+{
+  // Screen clear method
+    public static void clearScreen()
+    {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    public static void main(String args[]) throws Exception
+    {
+        System.out.println("\n\n\n--------* Welcome To IIITS Library *--------");
+        FirstPage p = new FirstPage();
+        Scanner sc = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("\n\n1. Admin Login\n2. Help\n3. Team Information\n4. Exit\n");
+            System.out.printf("Enter your choice : ");
+            int choice = sc.nextInt();
+            // For Login
+            switch(choice)
+            {
+                case 1:
+                {
+                    p.login();
+                    break;
+                }
+                // Help
+                case 2:
+                    System.out.println("Help information \n");
+                    break;
+                // Team details
+                case 3:
+                    p.teamInfo();
+                    break;
+                case 4 :
+                    {
+                        sc.close();
+                        return ;
+                    }
+                default:
+                    System.out.println("Please enter valid choice ");
+            }
+        }
+        
+    }
+}
